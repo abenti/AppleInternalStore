77ab324f42f5da8d5a2b167ad0b3ddef004c1a4f
Amanuel Benti <abenti@miu.edu>
2022-10-09 16:13:21 -0500

    initial commit

---
+++ src/main/java/com/example/appleinternalstore/AppleInternalStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.appleinternalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleInternalStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleInternalStoreApplication.class, args);
+    }
+
+}
