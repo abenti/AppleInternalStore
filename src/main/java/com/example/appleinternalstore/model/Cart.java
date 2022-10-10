57317bbc3fc7dd32c06644062078c54a4cf9927b
Amanuel Benti <abenti@miu.edu>
2022-10-09 21:53:32 -0500

    Create Cart Model

---
+++ src/main/java/com/example/appleinternalstore/model/Cart.java
@@ -0,0 +1,15 @@
+package com.example.appleinternalstore.model;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+public class Cart {
+    @Column(nullable=false, unique=true)
+    @NotBlank(message = "* Product List is required")
+    private List<Product> productList;
+
+    @Column(nullable=false, unique=true)
+    @NotBlank(message = "* User is required")
+    private User user;
+}
