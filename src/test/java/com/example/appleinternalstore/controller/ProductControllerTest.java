496f8dc8247295b93bf2b0af1e907090c8268828
Amanuel Benti <abenti@miu.edu>
2022-10-18 03:16:30 -0500

    added controller test and cleaned up application.properties

---
+++ src/test/java/com/example/appleinternalstore/controller/ProductControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.appleinternalstore.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnSuccessfulMessage() throws Exception {
+        this.mockMvc.perform(get("/products")
+                        .contentType("application/json")
+                        .param("category", "iphone"))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+}
